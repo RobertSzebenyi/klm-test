a06523c91ee2a78f42ffcfca9fa851a70de1e594
Robert Szebenyi <robert.szebenyi@lhsystems.com>
2024-11-22 08:46:46 +0100

    Initialize application: - create spring boot project with: spring-boot-starter-web, liquibase, and h2 database

---
+++ src/main/java/com/robert/szebenyi/klmtest/KlmTestApplication.java
@@ -0,0 +1,13 @@
+package com.robert.szebenyi.klmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robert/szebenyi/klmtest/KlmTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.robert.szebenyi.klmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class KlmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
